55b49c42cc55d9c650e365b0cf384ae54e44c506
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-09-21 15:34:43 -0400

    add income

---
+++ src/income/income.model.ts
@@ -0,0 +1,5 @@
+export enum IncomeError {
+  CouldNotGetIncomeById = 'Could not retrieved income. Income not found.',
+  CouldNotUpdateIncome = 'Could not update income. Income not found.',
+  CouldNotDeleteIncome = 'Could not delete income because Income does not exists.',
+}
